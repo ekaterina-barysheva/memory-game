4ab5c58a54ad753bd6adb3d1fcfb2513a7db41ac
Ekaterina Barysheva <barysheva.ek@gmail.com>
2020-06-08 14:32:30 +0200

    First commit

---
+++ game.js
@@ -0,0 +1,69 @@
+var userClickedPattern = [];
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var level = 0;
+var gameStarted = false;
+
+$(document).on("keypress", function() {
+  if (!gameStarted) {
+    gameStarted = true;
+    $("#level-title").html("Level " + level);
+    nextSequence();
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(nextSequence, 1000);
+    };
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  };
+}
+
+$(".btn").on("click", function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  var lastIndex = userClickedPattern.length - 1;
+  checkAnswer(lastIndex);
+});
+
+function nextSequence() {
+  level++;
+  $("#level-title").html("Level " + level);
+  userClickedPattern = [];
+  var randomNumber = Math.floor(Math.random() * 4);
+  //generates 0-3;
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  console.log(gameStarted, level);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  var gameStarted = false;
+};
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
